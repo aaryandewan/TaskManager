a5cf316d994deba6ff075a70b3e9b096a26b2d15
Aaryan Dewan <aaryan.dewan@iongroup.com>
2023-06-27 18:16:25 +0530

    Final working, still giving CORS error but I disabled them through an extension, idk why it's coming god i hate this

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const todoRoutes = require('./routes/todoRoutes');
+require("dotenv").config({ path: "../.env" });
+const express = require("express");
+const mongoose = require("mongoose");
+const todoRoutes = require("./routes/todoRoutes");
 
 const app = express();
 
 // Middleware
 app.use(express.json());
-
+let passw = process.env.MONGODB_PASSWORD;
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost/todo_app', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((error) => console.error('Failed to connect to MongoDB', error));
+mongoose
+  .connect(
+    "mongodb+srv://aaryan:" +
+      passw +
+      "@cluster0.ldq41v4.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error("Failed to connect to MongoDB", error));
 
 // Routes
-app.use('/todos', todoRoutes);
+app.use("/todos", todoRoutes);
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
-const app = require('./app');
-
+const app = require("./app");
+const express = require("express");
+const cors = require("cors");
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+
+// Apply the cors middleware
+app.use(cors({ origin: "http://127.0.0.1:5174" }));
+
+// app.use(cors({ origin: "http://127.0.0.1:5174" }));
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+//
